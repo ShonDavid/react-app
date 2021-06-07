9f793a92f6bbced0bf207374a8cd12dfeaf3c777
Shon David <shondavidhq@gmail.com>
2021-06-07 19:55:48 +0300

    tryouts for polyline

--- src/Components/BingMap.js
+++ src/Components/BingMap.js
@@ -1,19 +1,53 @@
 import './BingMap.css';
 import { ReactBingmaps } from 'react-bingmaps';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 function BingMap(props) {
+	const [pushPins, setPushPins] = useState([]);
+	const [polyline, setPolyLine] = useState([]);
+
+	useEffect(() => {
+		setPushPins(props.pins);
+		setPolyLine([
+			{
+				locations:
+					props.pins.length === 0
+						? []
+						: props.pins.map((pin) => {
+								let toRet = {
+									latitude: pin.location[0],
+									longitude: pin.location[1],
+								};
+								return toRet;
+						  }),
+				options: {
+					strokeColor: 'red',
+					visible: true,
+				},
+			},
+		]);
+	}, [props.pins]);
+
+	useEffect(() => {
+		console.log('polyline=', polyline);
+	}, [polyline]);
+
 	return (
 		<div className='BingMap'>
 			<ReactBingmaps
 				bingmapKey='AlLIyQyHrjClUlcXVt9dEdP6amZeKI13YhnNY9qbmasERZ5537U-Vb7TU-9a6887'
-				center={[13.0827, 80.2707]}
+				center={
+					pushPins.length === 0
+						? [10.0827, 70.2707]
+						: pushPins[pushPins.length - 1].location
+				}
 				// pushPins={[
 				// 	{
 				// 		location: [13.0827, 80.2707],
 				// 		option: { color: 'red' },
 				// 	},
 				// ]}
-				pushPins={props.pins}
+				PolyLine={polyline}
+				pushPins={pushPins}
 			></ReactBingmaps>
 		</div>
 	);
